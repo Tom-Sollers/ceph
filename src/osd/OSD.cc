d255bbe2982534e21ada2a9871294eedd7757666
Tom Sollers <tom.sollers@ibm.com>
2026-09-23 09:51:47 +0100

    osd: Update remove after umbrella to remove after vampire comment
    Following consulting with author, we don't yet want to remove.
    
    Signed-Off-By: Tom Sollers <tom.sollers@ibm.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -1767,7 +1767,7 @@ void OSDService::queue_for_snap_trim(PG *pg, uint64_t cost_per_object)
       return cost_per_object * cct->_conf->osd_pg_max_concurrent_snap_trims;
     } else {
       /* We retain this legacy behavior for WeightedPriorityQueue.
-       * This branch should be removed after Umbrella (after consulting
+       * This branch should be removed after Vampire (after consulting
        * dev team)
        */
       return cct->_conf->osd_snap_trim_cost;
